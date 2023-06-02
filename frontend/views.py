51e273e3923c41c13dfe76faa447e9e9432ed71e
Salahuddin Shaikh <salahoddin88@gmail.com>
2023-06-02 19:28:13 +0530

    Product listing and details page templating

--- frontend/views.py
+++ frontend/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from cms.models import Slider
 from product.models import ProductCategory, Product, ProductTag
 from cms.models import WebsiteSetting
+from django.views import View
 
 
 def home_page(request):
@@ -24,11 +25,52 @@ def home_page(request):
     }
     return render(request, 'home.html', context)
 
+""" Products with category id """
+# def product_listing(request, category_id):
+#     navigation_categories = ProductCategory.objects.filter(status=True)
+#     website_setting = WebsiteSetting.objects.all().last()
+#     # products = Product.objects.filter(status=True, product_category=category_id)
+#     products = Product.objects.filter(status=True, product_category_id=category_id) # recomended
+#     print(products)
+#     context = {
+#         'navigation_categories' : navigation_categories,
+#         'website_setting' : website_setting,
+#         'products' : products
+#     }
+#     return render(request, 'product/product_listing.html', context)
 
-def product_listing(request, category_id):
+def product_listing(request, product_category_slug):
+    """ Products with category slug """
     navigation_categories = ProductCategory.objects.filter(status=True)
-    product_category = ProductCategory.objects.get(id=category_id)
-    print(product_category)
-    return render(request, 'test.html', {'navigation_categories': navigation_categories})
+    website_setting = WebsiteSetting.objects.all().last()
+    products = Product.objects.filter(status=True, product_category__slug=product_category_slug) # recomended
+    context = {
+        'navigation_categories' : navigation_categories,
+        'website_setting' : website_setting,
+        'products' : products
+    }
+    return render(request, 'product/product_listing.html', context)
+
+
+
+class ProductDetails(View):
+
+    def get(self, request, product_slug):
+        navigation_categories = ProductCategory.objects.filter(status=True)
+        website_setting = WebsiteSetting.objects.all().last()
+        # product_slug = 'black-t-shirt'
+        try:
+            product = Product.objects.get(slug=product_slug)
+            similar_products = Product.objects.filter(status=True, product_category=product.product_category).exclude(id=product.id)
+            context = {
+                'navigation_categories' : navigation_categories,
+                'website_setting' : website_setting,
+                'product' : product,
+                'similar_products' : similar_products,
+            }
+            return render(request, 'product/product_details.html', context)
+        except Product.DoesNotExist:
+            pass
+
 
 
--- product/models.py
+++ product/models.py
@@ -67,7 +67,7 @@ class Product(models.Model):
 
 
 class ProductImage(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="ProductImage")
     image = models.ImageField(upload_to='products')
 
     def __str__(self):
